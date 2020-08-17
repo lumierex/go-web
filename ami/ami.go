0f996891982557dcb4170f82425cbb386fc2ca08
chanlumin <1025300632@qq.com>
2020-08-17 21:08:04 +0800

    feat(RouterGroup):  1. 添加路由分组

--- ami/ami.go
+++ ami/ami.go
@@ -1,6 +1,8 @@
 package ami
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -8,17 +10,70 @@ import (
 //type HandlerFunc func(http.ResponseWriter, *http.Request)
 type HandlerFunc func(c *Context)
 
-// ServeHTTP实现结构体
-type Engine struct {
-	router *router
-}
+type (
+	RouterGroup struct {
+		prefix      string
+		middlewares []HandlerFunc
+		parent      *RouterGroup
+		// 每个router-group共享一个engine的实例
+		engine *Engine
+	}
+
+	// ServeHTTP实现结构体
+
+	Engine struct {
+		// 嵌套属性 engine拥有RouterGroup属性
+		// 所以engine就有 group的Group属性
+		//
+		*RouterGroup
+		router *router
+		// Engine拥有所有的route-group
+		groups []*RouterGroup
+	}
+)
 
 // 引擎构造函数
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{
+		engine: engine,
+	}
+
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+	//return &Engine{router: newRouter()}
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	fmt.Printf("group prefix: %s, group: %v\n", group.prefix, group)
+	newGroup := &RouterGroup{
+		// 嵌套group
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+// comp -> pattern
+func (group *RouterGroup) addRoute(method, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	//engine.
 	engine.router.addRoute(method, pattern, handler)
 }
 
--- main.go
+++ main.go
@@ -43,24 +43,57 @@ func main() {
 	//	log.Fatal(err)
 	//}
 
+	//r := ami.New()
+	//
+	//r.GET("/", func(c *ami.Context) {
+	//	c.HTML(http.StatusOK, `<h1 style="color:red">hello world</h1>`)
+	//})
+	//
+	//r.GET("/hello", func(c *ami.Context) {
+	//	c.String(http.StatusOK, "hello %s, you are at %s \n", c.Path)
+	//})
+	//
+	//r.GET("/hello/:name", func(c *ami.Context) {
+	//	c.String(http.StatusOK, "hello %s you are at %s\n", c.Params["name"], c.Path)
+	//})
+	//
+	//r.GET("/assets/*filepath", func(c *ami.Context) {
+	//	c.JSON(http.StatusOK, ami.H{
+	//		"filepath": c.Param("filepath"),
+	//	})
+	//})
+	//_ = r.Run(":9999")
+
 	r := ami.New()
-
-	r.GET("/", func(c *ami.Context) {
-		c.HTML(http.StatusOK, `<h1 style="color:red">hello world</h1>`)
+	r.GET("/index", func(c *ami.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
-
-	r.GET("/hello", func(c *ami.Context) {
-		c.String(http.StatusOK, "hello %s, you are at %s \n", c.Path)
-	})
-
-	r.GET("/hello/:name", func(c *ami.Context) {
-		c.String(http.StatusOK, "hello %s you are at %s\n", c.Params["name"], c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *ami.Context) {
-		c.JSON(http.StatusOK, ami.H{
-			"filepath": c.Param("filepath"),
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *ami.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 		})
-	})
+
+		v1.GET("/hello", func(c *ami.Context) {
+			// expect /hello?name=amiktutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *ami.Context) {
+			// expect /hello/amiktutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *ami.Context) {
+			c.JSON(http.StatusOK, ami.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
+
 	_ = r.Run(":9999")
+
 }
